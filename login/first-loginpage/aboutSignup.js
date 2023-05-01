9ccb7a473f90ec0b63463573b14ef839874ec5fb
Myname <sun1497a@gmail.com>
2023-05-02 02:07:46 +0900

    re commit

--- login/first-loginpage/aboutSignup.js
+++ login/first-loginpage/aboutSignup.js
@@ -1,37 +1,40 @@
 const iname = document.getElementById('iname');
 const pname = document.getElementById('pname');
 const chname = document.getElementById('chname');
-const phone_name = document.getElementById('phone_name');
+const phone_num = document.getElementById('phone_name');
 const st_num = document.getElementById('st_num');
 
 
 const signup = document.querySelector('#signup');
-const cancel = document.querySelector('#cancel');
 
+const cancel = document.getElementById('cancel');
+
+const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 function getAllKL(){
-    if(iname.value!=='/^\w(?=@(?=[a-z](?=.(?=com))))/'){
-        console.log("1-1");
-        alert("1-1");
-        return;
+    if(re.test(String(iname.value))===false){
+        
+        alert("wrong imail address");
+        
     }
     if(chname.value!==pname.value){
-        console.log("2-2");
-        alert('2-2');
-        return;
+        
+        alert("diffrent password");
+        
     }
+    
     if(st_num.value.length!==10){
-        console.log("3-3");
-        alert('3-3');
-        return;
+        
+        alert("wrong studend num!");
+        
     }
-
-
+    
+    return 0;
 }
 
 
-cancel.addEventListener('click',function(){window.location.href="choice.html";});
-signup.addEventListener('cklck',function(){getAllKL();});
+cancel.addEventListener('click',function(){window.location.replace("/choice.html")});
+signup.addEventListener('click',function(){getAllKL()});
 
 
 
